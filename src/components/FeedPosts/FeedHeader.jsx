b055c17c619ea5b1b5e30fa7d1902e596c7cf4d2
dbstj589 <lysfox01@gmail.com>
2025-10-02 16:15:16 +0900

    feat : 메인페이지 게시물 조회 기능 구현

--- src/components/FeedPosts/FeedHeader.jsx
+++ src/components/FeedPosts/FeedHeader.jsx
@@ -1,12 +1,21 @@
+// src/components/FeedPosts/FeedHeader.jsx
 import React from "react";
 import "../../styles/FeedHeader.css";
 
-function FeedHeader({ username, avatar }) {
+function FeedHeader({ username, avatar, subtitle }) {
   return (
     <div className="feed-header">
       <div className="feed-header-left">
-        <img src={avatar} alt={username} className="feed-avatar" />
-        <span className="feed-username">{username}</span>
+        <img
+          src={avatar}
+          alt={username}
+          className="feed-avatar"
+          onError={(e) => (e.currentTarget.src = "/fallbacks/avatar_default.png")}
+        />
+        <div className="feed-name-wrap">
+          <span className="feed-username">{username}</span>
+          {subtitle && <span className="feed-subtitle">{subtitle}</span>}
+        </div>
       </div>
 
       <button className="unfollow-btn">Unfollow</button>
